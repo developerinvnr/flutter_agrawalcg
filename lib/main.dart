0d5ec6837e9d2bc863d41e58c5a469b961ff9e07
vnrit <vnrit@vnrits-mac-mini.local>
2021-11-23 18:05:07 +0530

    location added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:agrawalcg/file_manager.dart';
+import 'package:background_locator/location_dto.dart';
+import 'package:background_locator/settings/android_settings.dart';
+import 'package:background_locator/settings/ios_settings.dart';
+import 'package:background_locator/settings/locator_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-// import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:background_locator/background_locator.dart';
+import 'location_callback_handler.dart';
+import 'location_service_repository.dart';
 const kAndroidUserAgent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
 
 String selectedUrl = 'http://agrawalcg.com/';
@@ -28,12 +36,70 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final flutterWebViewPlugin = FlutterWebviewPlugin();
-
   DateTime currentBackPressTime = DateTime.now();
+
+  ReceivePort port = ReceivePort();
+  String logStr = '';
+  late bool isRunning;
+  late LocationDto lastLocation;
 
   @override
   void initState() {
     super.initState();
+    if (IsolateNameServer.lookupPortByName(LocationServiceRepository.isolateName) != null) {
+          IsolateNameServer.removePortNameMapping(LocationServiceRepository.isolateName);
+    }
+
+    IsolateNameServer.registerPortWithName(port.sendPort, LocationServiceRepository.isolateName);
+    port.listen((dynamic data) async {
+        await updateUI(data);
+      },
+    );
+    _startLocator().then((value){
+      initPlatformState();
+    });
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> updateUI(LocationDto data) async {
+    final log = await FileManager.readLogFile();
+
+    await _updateNotificationText(data);
+
+    setState(() {
+      if (data != null) {
+        lastLocation = data;
+      }
+      logStr = log;
+    });
+  }
+
+  Future<void> _updateNotificationText(LocationDto data) async {
+    if (data == null) {
+      return;
+    }
+
+    await BackgroundLocator.updateNotificationText(
+        title: "location Tracking",
+        msg: "${DateTime.now()}",
+        bigMsg: "${data.latitude}, ${data.longitude}");
+  }
+
+  Future<void> initPlatformState() async {
+    print('Initializing...');
+    await BackgroundLocator.initialize();
+    logStr = await FileManager.readLogFile();
+    print('Initialization done');
+    final _isRunning = await BackgroundLocator.isServiceRunning();
+    setState(() {
+      isRunning = _isRunning;
+    });
+    print('Running ${isRunning.toString()}');
   }
 
   Future<bool> _willpop() {
@@ -45,35 +111,6 @@ class _MyAppState extends State<MyApp> {
     }
     return Future.value(true);
   }
-
-
-  // _onBasicWaitingAlertPressed(context) async {
-  //   await Alert(
-  //       context: context,
-  //       title: "Are you sure you want to exit?",
-  //       image: Image.asset("assets/images/agrawalicon.png"),
-  //       buttons: [
-  //         DialogButton(
-  //           onPressed: () => Navigator.pop(context),
-  //           color: Colors.red,
-  //           child: Text(
-  //             "No",
-  //             style: TextStyle(color: Colors.white, fontSize: 20),
-  //           ),
-  //         ),
-  //         DialogButton(
-  //           onPressed: () => {
-  //             exit(0),
-  //           },
-  //           color: Colors.green,
-  //           child: Text(
-  //             "Yes",
-  //             style: TextStyle(color: Colors.white, fontSize: 20),
-  //           ),
-  //         )
-  //       ]
-  //   ).show();
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -97,4 +134,31 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  Future<void> _startLocator() async{
+    Map<String, dynamic> data = {'countInit': 1};
+    return await BackgroundLocator.registerLocationUpdate(LocationCallbackHandler.callback,
+        initCallback: LocationCallbackHandler.initCallback,
+        initDataCallback: data,
+        disposeCallback: LocationCallbackHandler.disposeCallback,
+        iosSettings: IOSSettings(
+            accuracy: LocationAccuracy.NAVIGATION, distanceFilter: 0),
+        autoStop: false,
+        androidSettings: AndroidSettings(
+            accuracy: LocationAccuracy.NAVIGATION,
+            interval: 30,
+            distanceFilter: 0,
+            client: LocationClient.google,
+            androidNotificationSettings: AndroidNotificationSettings(
+                notificationChannelName: 'Location tracking',
+                notificationTitle: 'Start Location Tracking',
+                notificationMsg: 'Track location in background',
+                notificationBigMsg:
+                'Background location is on to keep the app up-tp-date with your location. This is required for main features to work properly when the app is not running.',
+                notificationIconColor: Colors.grey,
+                notificationTapCallback:
+                LocationCallbackHandler.notificationCallback)));
+  }
 }
+
+
